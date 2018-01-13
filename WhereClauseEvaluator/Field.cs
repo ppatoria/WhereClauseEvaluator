42f25d2ad1d27bf1826ec88a6c9ae776cd48768a
Pralay Patoria <ppatoria@gmail.com>
2018-01-13 19:13:14 +0100

    minor refactoring

---
+++ WhereClauseEvaluator/Field.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhereClauseEvaluator
+{
+    public class Field : IEquatable<string>
+    {
+        public Field(string name, string value)
+        {
+            Name = name;
+            Value = value;
+        }
+        public string Name { get; }
+        public string Value { get; }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+        public override bool Equals(object other)
+        {
+            return Value.Equals(other);
+        }
+        public bool Equals(string other)
+        {
+            return Value.Equals(other);
+        }
+
+        public static bool operator ==(Field lhs, string rhs)
+        {
+            return lhs.Equals(rhs);
+        }
+        public static bool operator !=(Field lhs, string rhs)
+        {
+            return !lhs.Equals(rhs);
+        }
+
+        public static bool operator >(Field lhs, string rhs)
+        {
+            return
+                double.Parse(lhs.Value)
+                >
+                double.Parse(rhs);
+        }
+        public static bool operator <(Field lhs, string rhs)
+        {
+            return
+                double.Parse(lhs.Value)
+                <
+                double.Parse(rhs);
+        }
+
+        public static bool operator <=(Field lhs, string rhs)
+        {
+            return
+                double.Parse(lhs.Value)
+                <=
+                double.Parse(rhs);
+        }
+        public static bool operator >=(Field lhs, string rhs)
+        {
+            return
+                double.Parse(lhs.Value)
+                >=
+                double.Parse(rhs);
+        }
+    }
+
+}
---
+++ WhereClauseEvaluatorTests/FieldTests.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Linq.Expressions;
+using NUnit.Framework;
+using WhereClauseEvaluator;
+
+namespace WhereClauseEvaluatorTests
+{
+    [TestFixture]
+    class FieldTests
+    {
+        [TestCase("field1", "10", "10", ExpectedResult = true)]
+        [TestCase("field1", "10", "20", ExpectedResult = false)]
+        public bool EqualityTest(string fieldName, string fieldValue, string constValue)
+        {
+            var field = new Field(fieldName, fieldValue);
+            var expr = Expression.Equal(Expression.Constant(field), Expression.Constant(constValue));
+            return
+                Expression.Lambda<Func<bool>>(expr).Compile()()
+                &&
+                field.Equals(constValue);
+        }
+
+        [TestCase("field1", "10", "11", ExpectedResult = false)]
+        [TestCase("field1", "10", "10", ExpectedResult = false)]
+        [TestCase("field1", "11", "10", ExpectedResult = true)]
+        public bool GreaterThan_Operator_Test(string fieldName, string fieldValue, string constValue)
+        {
+            var field = new Field(fieldName, fieldValue);
+            var expr = Expression.GreaterThan(Expression.Constant(field), Expression.Constant(constValue));
+            return
+                Expression.Lambda<Func<bool>>(expr).Compile()()
+                &&
+                field > constValue;
+        }
+        [TestCase("field1", "11", "10", ExpectedResult = false)]
+        [TestCase("field1", "10", "10", ExpectedResult = false)]
+        [TestCase("field1", "10", "11", ExpectedResult = true)]
+        public bool LessThan_Operator_Test(string fieldName, string fieldValue, string constValue)
+        {
+            var field = new Field(fieldName, fieldValue);
+            var expr = Expression.LessThan(Expression.Constant(field), Expression.Constant(constValue));
+            return
+                Expression.Lambda<Func<bool>>(expr).Compile()()
+                &&
+                field < constValue;
+        }
+        [TestCase("field1", "10", "11", ExpectedResult = false)]
+        [TestCase("field1", "10", "10", ExpectedResult = true)]
+        [TestCase("field1", "11", "10", ExpectedResult = true)]
+        public bool GreaterThan_Or_Equal_Operator_Test(string fieldName, string fieldValue, string constValue)
+        {
+            var field = new Field(fieldName, fieldValue);
+            var expr = Expression.GreaterThanOrEqual(Expression.Constant(field), Expression.Constant(constValue));
+            return
+                Expression.Lambda<Func<bool>>(expr).Compile()()
+                &&
+                field >= constValue;
+        }
+        [TestCase("field1", "11", "10", ExpectedResult = false)]
+        [TestCase("field1", "10", "10", ExpectedResult = true)]
+        [TestCase("field1", "10", "11", ExpectedResult = true)]
+        public bool LessThan_Or_Equal_Operator_Test(string fieldName, string fieldValue, string constValue)
+        {
+            var field = new Field(fieldName, fieldValue);
+            var expr = Expression.LessThanOrEqual(Expression.Constant(field), Expression.Constant(constValue));
+            return
+                Expression.Lambda<Func<bool>>(expr).Compile()()
+                &&
+                field <= constValue;
+        }
+    }
+}
