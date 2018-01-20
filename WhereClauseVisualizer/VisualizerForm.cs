89245026be66d49e09f562c44befbeaf41e7af2b
Pralay Patoria <ppatoria@gmail.com>
2018-01-20 15:29:45 +0100

    rename

---
+++ WhereClauseVisualizer/VisualizerForm.cs
@@ -0,0 +1,148 @@
+﻿using ExpressionParser;
+using Lookup;
+using Moq;
+using SqlParser;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WhereClauseVisualizer
+{
+
+
+
+    public partial class WhereClauseVisualizer : Form
+    {
+        public WhereClauseVisualizer()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void WhereClauseTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void SetColor(TreeNode node, bool? result)
+        {
+            if (result == null)
+            {
+                throw new Exception("Invalid Data");
+            }
+            if (result.Value)
+            {
+                node.BackColor = Color.LightGreen;
+                
+            }
+            else
+            {
+                node.BackColor = Color.LightPink;
+            }
+        }
+        public void CreateBTree(List<object> prefix)
+        {
+            prefix.Reverse();
+            var stk = new Stack<TreeNode>(prefix.Count);
+            TreeNode newNode;
+            foreach (var node in prefix)
+            {
+                if (node is BinaryOperator)
+                {
+                    TreeNode childNode1 = stk.Pop();                    
+                    TreeNode childNode2 = stk.Pop();
+
+                    var op = (BinaryOperator)node;                    
+                    newNode = new TreeNode(op.Data);
+                    newNode.Nodes.Add(childNode1);
+                    newNode.Nodes.Add(childNode2);
+                    SetColor(newNode, op.Result);
+                    stk.Push(newNode);
+                }
+                else if (node is UnaryOperator)
+                {
+                    var op = (UnaryOperator)node;                    
+                    TreeNode childNode = stk.Pop();
+                    newNode = new TreeNode(op.Data);
+                    newNode.Nodes.Add(childNode);
+                    SetColor(newNode, op.Result);
+                    stk.Push(newNode);
+                }
+                else
+                {
+                    if(node is ColumnOperand)
+                    {
+                        var operand = (ColumnOperand)node;
+                        newNode = new TreeNode($"{operand.Data.Key}[{operand.Data.Value}]");
+                        stk.Push(newNode);
+                    }
+                    if(node is ConstantOperand)
+                    {
+                        var operand = (ConstantOperand)node;
+                        newNode = new TreeNode(operand.Data);
+                        stk.Push(newNode);
+
+                    }
+                    if(node is ConstantOperandOfList)
+                    {
+                        var operand = (ConstantOperandOfList)node;
+                        newNode = new TreeNode($"({string.Join(",",operand.Data)})");
+                        stk.Push(newNode);
+                    }
+                }
+
+            }
+            treeView.Nodes.Add(stk.Pop());
+        }
+
+        private void ViewCommand_Click(object sender, EventArgs e)
+        {
+            var whereClause = WhereClauseTextBox.Text.Trim();
+            var parser = new WhereClauseParser(whereClause);
+
+            var expr = parser.ToExpression(recordForLookup.Text.Trim());
+            var prefix = new ExpressionParser.ExpressionParser(expr)
+                .PrefixExpression;
+            CreateBTree(prefix);
+        }
+
+        private void ExpressionTree_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // WhereClauseVisualizer
+            // 
+            this.ClientSize = new System.Drawing.Size(282, 255);
+            this.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "WhereClauseVisualizer";
+            this.ResumeLayout(false);
+
+        }
+    }
+
+    public static class BinaryTree
+    {
+
+       
+    }
+}
---
+++ WhereClauseVisualizer/WhereClauseVisualizer.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace WhereClauseVisualizer
+{
+    partial class WhereClauseVisualizer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.treeView = new System.Windows.Forms.TreeView();
+            this.WhereClauseTextBox = new System.Windows.Forms.RichTextBox();
+            this.ViewCommand = new System.Windows.Forms.Button();
+            this.recordForLookup = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // treeView
+            // 
+            this.treeView.Location = new System.Drawing.Point(0, 0);
+            this.treeView.Name = "treeView";
+            this.treeView.Size = new System.Drawing.Size(677, 1090);
+            this.treeView.TabIndex = 0;
+            this.treeView.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.ExpressionTree_AfterSelect);
+            // 
+            // WhereClauseTextBox
+            // 
+            this.WhereClauseTextBox.Location = new System.Drawing.Point(683, 38);
+            this.WhereClauseTextBox.Name = "WhereClauseTextBox";
+            this.WhereClauseTextBox.Size = new System.Drawing.Size(1117, 96);
+            this.WhereClauseTextBox.TabIndex = 1;
+            this.WhereClauseTextBox.Text = "";
+            this.WhereClauseTextBox.TextChanged += new System.EventHandler(this.WhereClauseTextBox_TextChanged);
+            // 
+            // ViewCommand
+            // 
+            this.ViewCommand.Location = new System.Drawing.Point(961, 306);
+            this.ViewCommand.Name = "ViewCommand";
+            this.ViewCommand.Size = new System.Drawing.Size(75, 23);
+            this.ViewCommand.TabIndex = 2;
+            this.ViewCommand.Text = "View";
+            this.ViewCommand.UseVisualStyleBackColor = true;
+            this.ViewCommand.Click += new System.EventHandler(this.ViewCommand_Click);
+            // 
+            // recordForLookup
+            // 
+            this.recordForLookup.Location = new System.Drawing.Point(683, 162);
+            this.recordForLookup.Name = "recordForLookup";
+            this.recordForLookup.Size = new System.Drawing.Size(1117, 96);
+            this.recordForLookup.TabIndex = 3;
+            this.recordForLookup.Text = "";
+            this.recordForLookup.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // WhereClauseVisualizer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1980, 1093);
+            this.Controls.Add(this.recordForLookup);
+            this.Controls.Add(this.ViewCommand);
+            this.Controls.Add(this.WhereClauseTextBox);
+            this.Controls.Add(this.treeView);
+            this.Name = "WhereClauseVisualizer";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView treeView;
+        private System.Windows.Forms.RichTextBox WhereClauseTextBox;
+        private System.Windows.Forms.Button ViewCommand;
+        private System.Windows.Forms.RichTextBox recordForLookup;
+    }
+}
+
